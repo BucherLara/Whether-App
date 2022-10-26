a3905240a1430ebf340c08ea177451af2f9a75f0
Lara Bucher <bucher.lara@web.de>
2022-10-26 12:49:49 +0200

    colorful weather wethers

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,9 +1,28 @@
+import styled, { css } from "styled-components";
+
 export default function Todo({ todo }) {
   return (
     <>
-      <li>
-        {todo.title} {todo.weather}
-      </li>
+      <ListItem weather={todo.weather}>{todo.title}</ListItem>
     </>
   );
 }
+
+const ListItem = styled.li`
+  ${({ weather }) =>
+    weather === "good" &&
+    css`
+      background-color: yellow;
+    `}
+  ${({ weather }) =>
+    weather === "bad" &&
+    css`
+      background-color: blue;
+    `}
+    ${({ weather }) =>
+    weather !== "bad" &&
+    weather !== "good" &&
+    css`
+      background-color: green;
+    `}
+`;
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ const todos = [
   { id: 1, title: "Eat tacos", isChecked: false, weather: "good" },
   { id: 2, title: "Brew tea", isChecked: false, weather: "always" },
   { id: 3, title: "Bad Todo", isChecked: false, weather: "bad" },
-  { id: 4, title: "Good Todo", isChecked: false, weather: "good" },
+  { id: 4, title: "Good Todo", isChecked: false, weather: "stormy" },
   { id: 5, title: "Always Todo", isChecked: false, weather: "always" },
 ];
 
